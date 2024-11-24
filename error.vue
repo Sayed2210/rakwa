6aa7402ca3b2e24fbbfb7a35478f7c21fa9121d4
sayed2210 <eh60039@gmail.com>
2024-11-24 20:35:23 +0200

    start with page error

---
+++ error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+  <NuxtLayout name="default">
+    fdghsaf
+  </NuxtLayout>
+</template>
