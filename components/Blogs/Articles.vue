bacb431130cc4ea88c204d836dd8e212b5f62b83
sayed2210 <eh60039@gmail.com>
2024-11-29 15:43:58 +0200

    blog card

--- components/Blogs/Articles.vue
+++ components/Blogs/Articles.vue
@@ -2,7 +2,32 @@
 
 <template>
   <section class="blogs">
-    <h1>{{ $t("All_Articles") }}</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div class="col-span-1 md:col-span-2 lg:col-span-3">
+        <div class="flex justify-between items-center">
+          <h1 class="title">{{ $t("All_Articles") }}</h1>
+          <div class="input-search">
+            <div class="search-icon">
+              <IconsSearch />
+            </div>
+            <input type="text" placeholder="Search by title" />
+          </div>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div class="col-span-1" v-for="i in 6" :key="i">
+            <BlogsBlogCard />
+          </div>
+        </div>
+      </div>
+      <div class="col-span-1">
+        <div class="filter">
+          <div class="filter-icon">
+            <IconsFilter />
+          </div>
+          <div class="filter-title">{{ $t("Filter") }}</div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
---
+++ components/Blogs/BlogCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="blog-card">
+    <div class="blog-img">
+      <NuxtImg
+        src="/blog-card.png"
+        alt="blog img"
+        class="blog-img"
+        format="webp"
+      />
+    </div>
+    <div class="blog-info">
+      <div class="flex items-start gap-4">
+        <span class="line"></span>
+        <div class="info-wrapper">
+          <div class="flex items-center gap-4">
+            <h2 class="blog-category">consulting</h2>
+            <span class="dot"></span>
+            <span class="date">November 7, 2024</span>
+          </div>
+          <h3 class="blog-title">
+            Our main goal is to "develop yourself as a leader"
+          </h3>
+          <p class="blog-description">
+            Chicago's next public school budget will depend on $500 million in
+            retirement savings that have not yet been approved by Illinois,
+            school officials said Monday.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
