d2f78947132d87abfd2d3b6011932575ff0aff60
sayed2210 <eh60039@gmail.com>
2024-12-10 21:56:01 +0200

    listing details

---
+++ components/ListingDetails/Gallery.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const images = [
+  "/ehud-neuhaus-dP1To-YckZI-unsplash.jpg",
+  "/ehud-neuhaus-TmJl4_5cSGs-unsplash.jpg",
+  "/ehud-neuhaus-zRRlQ2JEK-M-unsplash.jpg",
+  "/hans-vivek-LcJZ1J9rF10-unsplash.jpg",
+  "/40171.png",
+];
+</script>
+
+<template>
+  <div class="img-gallery">
+    <NuxtImg
+      v-for="image in images"
+      :src="image"
+      alt="listing image"
+      :class="[
+        image === images[0] ? 'main-img' : '',
+      ]"
+      format="webp"
+
+    />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/listing/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="listing-details">
+    <ListingDetailsBanner />
+    <ListingDetailsContent />
+  </div>
+</template>
+
+<style scoped></style>
